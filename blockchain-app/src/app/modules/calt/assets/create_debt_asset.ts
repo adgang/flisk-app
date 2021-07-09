d0c391d94720af6d6041a7598626c65ac33e0245
Aditya Gangipamula <adgang@tuta.io>
2021-07-09 17:14:48 +0530

    created asset transaction to create debt and validate the txn

--- blockchain-app/src/app/modules/calt/assets/create_debt_asset.ts
+++ blockchain-app/src/app/modules/calt/assets/create_debt_asset.ts
@@ -2,23 +2,47 @@ import { BaseAsset, ApplyAssetContext, ValidateAssetContext } from 'lisk-sdk';
 
 export class CreateDebtAsset extends BaseAsset {
 	public name = 'createDebt';
-  public id = 4;
+	public id = 4;
 
-  // Define schema for asset
+	// Define schema for asset
 	public schema = {
-    $id: 'calt/createDebt-asset',
+		$id: 'calt/createDebt-asset',
 		title: 'CreateDebtAsset transaction asset for calt module',
 		type: 'object',
-		required: [],
-		properties: {},
-  };
+		required: ['minPurchaseMargin', 'maturityValue', 'name', 'maturityDate'],
+		properties: {
+			minPurchaseMargin: {
+				dataType: 'uint32',
+				fieldNumber: 1,
+			},
+			maturityValue: {
+				dataType: 'uint32',
+				fieldNumber: 2,
+			},
+			name: {
+				dataType: 'string',
+				fieldNumber: 3,
+			},
+			// unixtime ms of maturity date
+			maturityDate: {
+				dataType: 'uint64',
+				fieldNumber: 4,
+			},
+		},
+	};
 
-  public validate({ asset }: ValidateAssetContext<{}>): void {
-    // Validate your asset
-  }
+	public validate({ asset }: any): void {
+		// Validate your asset
+		// Debt has to be created only for future maturity
+		if (asset.maturityDate < Date.now() + 100000) {
+			throw new Error("Debt's maturity should be in future.");
+		} else if (asset.minPurchaseMargin < 0) {
+			throw new Error('The debt minimum purchase value needs to be above 0.');
+		}
+	}
 
 	// eslint-disable-next-line @typescript-eslint/require-await
-  public async apply({ asset, transaction, stateStore }: ApplyAssetContext<{}>): Promise<void> {
+	public async apply({ asset, transaction, stateStore }: ApplyAssetContext<{}>): Promise<void> {
 		throw new Error('Asset "createDebt" apply hook is not implemented.');
 	}
 }
