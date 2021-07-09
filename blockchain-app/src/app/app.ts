ec334ce48def1a7eff96ad6d451882c82fd61881
Aditya Gangipamula <adgang@tuta.io>
2021-07-09 19:43:39 +0530

    added fields to accounts of genesis block

--- blockchain-app/src/app/app.ts
+++ blockchain-app/src/app/app.ts
@@ -1,12 +1,32 @@
-import { Application, PartialApplicationConfig } from 'lisk-sdk';
+import { Application, PartialApplicationConfig, utils } from 'lisk-sdk';
 import { registerModules } from './modules';
 import { registerPlugins } from './plugins';
 
 export const getApplication = (
-	genesisBlock: Record<string, unknown>,
+	genesisBlock: Record<string, any>,
 	config: PartialApplicationConfig,
 ): Application => {
-	const app = Application.defaultApplication(genesisBlock, config);
+	// 3.Update the genesis block accounts to include NFT module attributes
+	genesisBlock.header.timestamp = 1605699440;
+	genesisBlock.header.asset.accounts = genesisBlock.header.asset.accounts.map(a =>
+		utils.objects.mergeDeep({}, a, {
+			calt: {
+				ownAssets: [],
+				ownLiabilities: [],
+			},
+		}),
+	);
+
+	// 4.Update application config to include unique label
+	// and communityIdentifier to mitigate transaction replay
+	const appConfig = utils.objects.mergeDeep({}, config, {
+		label: 'nft-app',
+		genesisConfig: { communityIdentifier: 'NFT' }, //In order to have a unique networkIdentifier
+		logger: {
+			consoleLogLevel: 'info',
+		},
+	});
+	const app = Application.defaultApplication(genesisBlock, appConfig);
 
 	registerModules(app);
 	registerPlugins(app);
